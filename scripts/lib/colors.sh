ab6b6fd3eb259166022cc5bcb54e80e361a746c1
alexandre lababsa <a.lababsa@lababsa.com>
2026-02-23 21:13:27 +0100

    init

---
+++ scripts/lib/colors.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Color definitions for consistent output across scripts
+export RED='\033[0;31m'
+export GREEN='\033[0;32m'
+export YELLOW='\033[1;33m'
+export BLUE='\033[0;34m'
+export PURPLE='\033[0;35m'
+export CYAN='\033[0;36m'
+export WHITE='\033[1;37m'
+export NC='\033[0m' # No Color
